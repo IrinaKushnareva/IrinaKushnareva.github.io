a808d8f2afe1dcf608e8ded26a89e4b8a01c8e2f
Irina Kushnareva <kushnareva.irina@gmail.com>
2016-11-23 22:08:47 +0400

    scroll

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,37 +1,25 @@
-/**
- * Created by koste on 01.06.2016.
- */
-
-//NAV FIXED
-var h_hght = 400;
-var h_mrg = 0;
-
 $(function(){
-
-    var elem = $('#top_nav');
-    var top = $(this).scrollTop();
-
-    if(top > h_hght){
-        elem.css('top', h_mrg);
-    }
-
-    $(window).scroll(function(){
-        top = $(this).scrollTop();
-        if (top+h_mrg < h_hght) {
-            elem.css('top', (h_hght-top));
-        } else {
-            elem.css('top', h_mrg);
-        }
+    //scroll
+    $("body").on("click", "a.topLink", function(event){
+      event.preventDefault();
+      var name = $(this).attr('href').substring(1),
+      top = $('[name="'+name+'"]').offset().top;
+      $('html,body').animate({scrollTop: top},
+        {
+                 duration: 700,
+                 easing: "linear"
+      });
     });
-    $("#bs-example-navbar-collapse-2").on("click", "a", function(event){
-        $('#bs-example-navbar-collapse-2').collapse('hide');
-       // event.preventDefault();
-    });
-    //
-    $("#nav_item").on("click","a", function (event) {
-        event.preventDefault();
-        var name = $(this).attr('href').substring(1),
-            top = $('[name="'+name+'"]').offset().top;
-        $('body,html').animate({scrollTop: top}, 700);
-    });
+
+    $("body").on("click", "a.btn", function(event){
+      event.preventDefault();
+      var name = $(this).attr('href').substring(1),
+      top = $('[name="'+name+'"]').offset().top;
+      $('html,body').animate({scrollTop: top},
+        {
+                 duration: 700,
+                 easing: "linear"
+        });
+      });
+
 });
